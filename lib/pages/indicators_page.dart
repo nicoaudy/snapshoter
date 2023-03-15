8b7d3ebc74e1cbda9d4c5d2de943f7cf3f2aee2d
Nico Audy <nicoaudy1994@gmail.com>
2023-03-15 14:46:37 +0700

    initial commit

---
+++ lib/pages/indicators_page.dart
@@ -0,0 +1,46 @@
+import 'package:macos_ui/macos_ui.dart';
+// ignore: implementation_imports
+import 'package:macos_ui/src/library.dart';
+
+class IndicatorsPage extends StatefulWidget {
+  const IndicatorsPage({super.key});
+
+  @override
+  State<IndicatorsPage> createState() => _IndicatorsPageState();
+}
+
+class _IndicatorsPageState extends State<IndicatorsPage> {
+  double ratingValue = 0;
+  double capacitorValue = 0;
+  double sliderValue = 0.3;
+
+  @override
+  Widget build(BuildContext context) {
+    return MacosScaffold(
+      toolBar: ToolBar(
+        title: const Text('TBD'),
+        titleWidth: 150.0,
+        actions: [
+          ToolBarIconButton(
+            label: 'Toggle Sidebar',
+            icon: const MacosIcon(CupertinoIcons.sidebar_left),
+            onPressed: () => MacosWindowScope.of(context).toggleSidebar(),
+            showLabel: false,
+          ),
+        ],
+      ),
+      children: [
+        ContentArea(
+          builder: (context, scrollController) {
+            return SingleChildScrollView(
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                children: const [Center(child: Text('Hello world'))],
+              ),
+            );
+          },
+        ),
+      ],
+    );
+  }
+}
